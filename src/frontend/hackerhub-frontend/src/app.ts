1b66b92bd1abdfe2ac5e41744c83ec35a99a254b
mwg <matt.george.89@gmail.com>
2017-05-04 11:27:19 +0100

    Add people component, and nav

--- src/frontend/hackerhub-frontend/src/app.ts
+++ src/frontend/hackerhub-frontend/src/app.ts
@@ -6,8 +6,10 @@ export class App {
       config.map([        
           { route: ['','home'],  name: 'home', nav: true ,
             moduleId: './components/home/home', title:'Home' },
-          { route: 'ideas',  name: 'idea-generator', nav: true,
-            moduleId: './components/idea-generator/idea-generator', title:'Idea Generator' }
+          { route: 'ideas',  name: 'ideas', nav: true,
+            moduleId: './components/idea-generator/idea-generator', title:'Idea Generator' },
+          { route: 'people',  name: 'people', nav: true,
+            moduleId: './components/people/people', title:'People' }
       ]);
    }
 }
---
+++ src/frontend/hackerhub-frontend/src/components/people/people.ts
@@ -0,0 +1,5 @@
+export class People {
+  people = ['Matt', 'Rick', 'Dave'];
+  constructor() {
+   }
+}
