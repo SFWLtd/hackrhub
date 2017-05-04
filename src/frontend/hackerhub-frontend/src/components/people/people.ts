dff3a38cb3a75aeea44980972b6d75e4a1dac8de
mwg <matt.george.89@gmail.com>
2017-05-04 11:42:40 +0100

    Restyle

--- src/frontend/hackerhub-frontend/src/components/people/people.ts
+++ src/frontend/hackerhub-frontend/src/components/people/people.ts
@@ -1,5 +1,5 @@
 export class People {
-  people = [{name: 'Matt', img: '/static/img/mg.jpg'}, {name: 'Rick', img: '/static/img/rp.jpg'}, {name: 'Dave', img: '/static/img/dr.png'}];
+  people = [{name: 'Matt', img: '/static/img/mg.jpg', team: 'Toni & Guys'}, {name: 'Rick', img: '/static/img/rp.jpg', team: 'Toni & Guys'}, {name: 'Dave', img: '/static/img/dr.png', team: 'Toni & Guys'}];
   constructor() {
    }
 }
