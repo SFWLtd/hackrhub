96bc461704678098af05d72c5c880bb68f94aac4
Rick Powell <richard.powell@sfwltd.co.uk>
2017-05-04 18:10:57 +0100

    Choose team to vote for

--- src/frontend/hackerhub-frontend/src/components/vote/vote.ts
+++ src/frontend/hackerhub-frontend/src/components/vote/vote.ts
@@ -1,4 +1,43 @@
 export class Vote {
+  teams: any;
+  votingForTeamId: number;
+
   constructor() {
+    this.votingForTeamId = 0;
+  }
+
+  attached() {
+    let stupidHack = this;
+    fetch('https://civica-hackathon-api.azurewebsites.net/api/teams', {
+        method: 'GET',
+        mode: 'cors'
+      }).then(function (response) {
+          response.json().then(function(data) {
+
+              let teamNumber: number = 1;
+              data = data.filter(function(team) {
+                  let canVoteForTeam: boolean = true;
+                  team.People.forEach(p => {
+                    if (p.Name === localStorage['name']) {
+                      canVoteForTeam = false;
+                    }
+                  });
+
+                  if (canVoteForTeam && team.Name !== "Observers") {
+                     team.Number = teamNumber;
+                     teamNumber++;
+                     return true;
+                  }
+
+                  return false;
+              });
+
+              stupidHack.teams = data;
+          });
+      });
+    }
+
+   voteForTeam(teamId: number) {
+      this.votingForTeamId = teamId;
    }
-}
+  }
