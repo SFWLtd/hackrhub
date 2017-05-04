023fa3cc148fd5f7dea16af3ad7b8e001944fd1a
Simon <simonk@sfwltd.co.uk>
2017-05-04 09:43:58 +0100

    Backend commits

---
+++ src/backend/src/main/kotlin/com/civica/hackrhub/HackrhubApplication.kt
@@ -0,0 +1,19 @@
+package com.civica.hackrhub
+
+import io.dropwizard.Application
+import io.dropwizard.Configuration
+import io.dropwizard.setup.Environment
+
+/**
+ * Created by SKX on 04/05/2017.
+ */
+class HackrhubApplication: Application<HackrhubConfig>() {
+    override fun run(configuration: HackrhubConfig?, environment: Environment?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
+
+class HackrhubConfig : Configuration() {
+
+}
