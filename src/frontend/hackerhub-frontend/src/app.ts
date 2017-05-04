20521ac9b6d85dccc12cf00ce9a61fbce0d2bc11
mwg <matt.george.89@gmail.com>
2017-05-04 12:38:18 +0100

    Add teams page

--- src/frontend/hackerhub-frontend/src/app.ts
+++ src/frontend/hackerhub-frontend/src/app.ts
@@ -1,15 +1,17 @@
 export class App {
   router: any;
   configureRouter(config, router){
-      config.title = 'Aurelia';		
+      config.title = 'Hackrhub';		
       config.options.pushState = true;
       config.map([        
           { route: ['','home'],  name: 'home', nav: true ,
             moduleId: './components/home/home', title:'Home' },
           { route: 'ideas',  name: 'ideas', nav: true,
             moduleId: './components/idea-generator/idea-generator', title:'Idea Generator' },
-          { route: 'people',  name: 'people', nav: true,
-            moduleId: './components/people/people', title:'People' }
+          { route: 'hackrs',  name: 'hackrs', nav: true,
+            moduleId: './components/people/people', title:'Hackrs' },
+          { route: 'teams',  name: 'teams', nav: true,
+            moduleId: './components/teams/teams', title:'Teams' }
       ]);
    }
 }
---
+++ src/frontend/hackerhub-frontend/src/components/teams/teams.ts
@@ -0,0 +1,29 @@
+export class Teams {
+
+    teams = [
+        {
+            name: "Left4Dev",
+            location: "Belfast",
+            description: "We are developing Name of service to help (target users/solve a problem) with (our secret source).",
+            people: [
+                {name: "Niall", img: '/static/img/mg.jpg'},
+                {name: "Andrew", img: '/static/img/mg.jpg'}
+            ]
+        },
+        {
+            name: "Toni & Guys",
+            location: "Woking",
+            description: "We are developing Name of service to help (target users/solve a problem) with (our secret source).",
+            people: [
+                {name: "Matt", img: '/static/img/mg.jpg'},
+                {name: "Rick", img: '/static/img/mg.jpg'},
+                {name: "Dave", img: '/static/img/mg.jpg'},
+                {name: "Simon", img: '/static/img/mg.jpg'},
+                {name: "Toni", img: '/static/img/mg.jpg'}
+            ]
+        }
+    ]
+
+  constructor() {
+   }
+}
