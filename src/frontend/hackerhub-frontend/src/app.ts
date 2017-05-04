14ecef12b207a5ba1b33765e28955444a545c52f
mwg <matt.george.89@gmail.com>
2017-05-04 10:09:30 +0100

    Remove hashbang from routes

--- src/frontend/hackerhub-frontend/src/app.ts
+++ src/frontend/hackerhub-frontend/src/app.ts
@@ -2,7 +2,8 @@ export class App {
   router: any;
   configureRouter(config, router){
       config.title = 'Aurelia';		
-      config.map([
+      config.options.pushState = true;
+      config.map([        
           { route: ['','home'],  name: 'home', nav: true ,
             moduleId: './components/home/home', title:'Home' },
           { route: 'ideas',  name: 'idea-generator', nav: true,
