8c6da314fa72d1bb94dde4e3ba182cabf5863cbe
David Raine <David.Raine@civica.co.uk>
2017-05-12 16:07:08 +0100

    Detect faces in twitter media

--- src/backend2/HackRHub/HackRHub/Controllers/TwitterController.cs
+++ src/backend2/HackRHub/HackRHub/Controllers/TwitterController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -7,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 using HackRHub.Models;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.ProjectOxford.Face;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -16,6 +19,12 @@ namespace HackRHub.Controllers
     {
         static string consumerKey = System.Configuration.ConfigurationManager.AppSettings["TwitterConsumerKey"];
         static string consumerSecret = System.Configuration.ConfigurationManager.AppSettings["TwitterConsumerSecret"];
+
+        static string apiKey = System.Configuration.ConfigurationManager.AppSettings["FaceApiKey"];
+        static string personGroupId = "civicans";
+
+        static string dbKey = System.Configuration.ConfigurationManager.AppSettings["DocumentDbKey"];
+        static string dbEndpoint = System.Configuration.ConfigurationManager.AppSettings["DocumentDbEndpoint"];
 
         [HttpGet]
         [Route("api/twitter/tweets")]
@@ -53,20 +62,66 @@ namespace HackRHub.Controllers
 
             var tweets = new List<Tweet>();
 
+            var faceServiceClient = new FaceServiceClient(apiKey, "https://westeurope.api.cognitive.microsoft.com/face/v1.0");
+
             foreach (dynamic status in obj.statuses)
             {
-                tweets.Add(new Tweet
+                Tweet tweet = new Tweet
                 {
                     Text = status.full_text,
                     UserName = status.user.name,
                     UserScreenName = status.user.screen_name,
                     HasMedia = status?.entities?.media?.HasValues ?? false,
-                    MediaUrls = ((JArray)status?.extended_entities?.media)?.Select(x => x["media_url"].ToString()).ToArray() ?? new string[0],
-                    People = new Person[0]
-                });
+                    MediaUrls = ((JArray)status?.extended_entities?.media)?.Select(x => x["media_url"].ToString()).ToArray() ?? new string[0]
+                };
+
+                var peopleInTweet = new List<Person>();
+
+                foreach (var url in tweet.MediaUrls)
+                {
+                    var image = await client.GetStreamAsync(url);
+                    var people = await GetPeopleInPicture(faceServiceClient, image);
+                    peopleInTweet.AddRange(people);
+                }
+
+                tweet.People = peopleInTweet.ToArray();
+
+                tweets.Add(tweet);
             }
 
             return Ok(tweets);
+        }
+
+        private static async Task<IEnumerable<Person>> GetPeopleInPicture(FaceServiceClient faceServiceClient, Stream image)
+        {
+            var result = new List<Person>();
+
+            var faces = await faceServiceClient.DetectAsync(image);
+            var faceIds = faces.Select(face => face.FaceId).ToArray();
+
+            if (faceIds.Any())
+            {
+                var results = await faceServiceClient.IdentifyAsync(personGroupId, faceIds);
+
+                var queryOptions = new FeedOptions { MaxItemCount = 1 };
+                var client = new DocumentClient(new Uri(dbEndpoint), dbKey);
+
+                foreach (var identifyResult in results)
+                {
+                    if (identifyResult.Candidates.Length != 0)
+                    {
+                        // Get top 1 among all candidates returned
+                        var candidateId = identifyResult.Candidates[0].PersonId;
+                        var person = client.CreateDocumentQuery<Person>(UriFactory.CreateDocumentCollectionUri("ToDoList", "Items"),
+                            $"SELECT * FROM  root r WHERE r.entityType = 'user' AND r.personId = '{candidateId}'",
+                            queryOptions).AsEnumerable().FirstOrDefault();
+
+                        result.Add(person);
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
